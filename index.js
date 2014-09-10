4dd9fe646bd5a65d76f21e8ced360f57c1204f70
Rebecca Turner <turner@mikomi.org>
2014-09-10 15:34:07 -0700

    Fix horrible ugly bug where it was resolving requires relative to itself rather than its caller

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 "use strict";
+var path = require("path")
 
 module.exports = function (toLoad, mocks) {
   // Inject all of our mocks
@@ -7,12 +8,15 @@ module.exports = function (toLoad, mocks) {
     require.cache[path] = {exports: mocks[name]}
   })
 
+  if (/^[.][.]?\//.test(toLoad)) {
+      toLoad = path.resolve(path.dirname(module.parent.filename), toLoad)
+  }
   var toLoadPath = require.resolve(toLoad)
 
   // remove any unmocked version previously loaded
   delete require.cache[toLoadPath]
   // load our new version using our mocks
-  var mocked = require(toLoadPath)
+  var mocked = module.parent.require(toLoadPath)
   // remove our version from the cache so anyone else gets the real thing
   delete require.cache[toLoadPath]
 
