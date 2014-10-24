682864535651230e0f788a211a4303992bdf572b
Rebecca Turner <turner@mikomi.org>
2014-10-24 01:43:49 -0400

    Add tests for fix for #1 (thanks @nathasm!)

---
+++ test/injection-leaking-1.js
@@ -0,0 +1,21 @@
+"use strict";
+var test = require("tap").test
+var requireInject = require("../index")
+
+
+test("injection leaking at a distance", function(t) {
+  t.plan(2)
+
+  var first = requireInject("./lib/a", {
+    "fs": {
+      rename: function(infile, outfile, cb) {
+        cb()
+      }
+    }
+  })
+  first("in", "out", function (err) { t.notOk(err,"should be able to rename a file") })
+
+  var second = require("./lib/a");
+
+  second("in", "out", function (err) { t.ok(err,"shouldn\'t be able to rename now") })
+})
