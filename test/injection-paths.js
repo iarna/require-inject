547c07b499af56b9224c6db2cd854290b9c68e3b
Rebecca Turner <me@re-becca.org>
2016-06-03 15:29:18 -0700

    Tacksify inject-paths test so that it works w/o fixtures

--- test/injection-paths.js
+++ test/injection-paths.js
@@ -1,13 +1,45 @@
 'use strict'
-
+var path = require('path')
 var test = require('tap').test
+var Tacks = require('tacks')
+var File = Tacks.File
+var Dir = Tacks.Dir
 var requireInject = require('../index')
+
+var testdir = path.join(__dirname, path.basename(__filename, '.js'))
+var adir = path.join(testdir, 'a')
+var bdir = path.join(testdir, 'b')
+var brelative = './' + path.relative(__dirname, bdir)
+
+var fixture = new Tacks(
+  Dir({
+    'a.js': File(
+      "'use strict';\n" +
+      "var b = require('./b');\n" +
+      'module.exports = function(infile, outfile, cb) {\n' +
+      '  b(infile, outfile, cb);\n' +
+      '};\n'
+    ),
+    'b.js': File(
+      "'use strict';\n" +
+      "var fs = require('fs');\n" +
+      'module.exports = function(infile, outfile, cb) {\n' +
+      '  fs.rename(infile, outfile, cb)\n' +
+      '};\n'
+    )
+  })
+)
+
+test('setup', function (t) {
+  fixture.create(testdir)
+  t.end()
+})
 
 test('mock with absolute path', function (t) {
   t.plan(1)
 
-  var a = requireInject('./lib/a', {
-    [require.resolve('./lib/b')]: function (infile, outfile, cb) {
+  var a = requireInject(adir, {
+    [bdir]: function (infile, outfile, cb) {
       cb()
     }
   })
@@ -20,8 +52,8 @@ test('mock with absolute path', function (t) {
 test('mock with relative path', function (t) {
   t.plan(1)
 
-  var a = requireInject('./lib/a', {
-    './lib/b': function (infile, outfile, cb) {
+  var a = requireInject(adir, {
+    [brelative]: function (infile, outfile, cb) {
       cb()
     }
   })
@@ -29,4 +61,9 @@ test('mock with relative path', function (t) {
   a('in', 'out', function (err) {
     t.notOk(err, 'should be able to rename a file')
   })
+})
+
+test('cleanup', function (t) {
+  fixture.remove(testdir)
+  t.end()
 })
